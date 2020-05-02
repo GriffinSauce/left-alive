4283812b7896776b830308c1b4b58542cb154b51
Joris Griffioen <jorisjgriffioen@gmail.com>
2020-05-02 11:36:03 +0200

    Remove GA

--- pages/index.js
+++ pages/index.js
@@ -131,7 +131,7 @@ export default function Home() {
                 <div className="button-attachment">
                   Like us to get the latest news on shows and other adventures!
                 </div>
-                <div className="btn-after facebook-feed border-t border-grey-100">
+                <div className="border-t btn-after facebook-feed border-grey-100">
                   <FacebookFeed />
                 </div>
               </div>
@@ -172,8 +172,6 @@ export default function Home() {
               </p>
             </div>
           </div>
-
-          <div className="clearfix" />
         </div>
       </div>
       <div>
@@ -189,16 +187,6 @@ export default function Home() {
           </p>
         </footer>
       </div>
-
-      {/* <!-- analytics -->
-		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
-		</script> */}
     </>
   );
 }
