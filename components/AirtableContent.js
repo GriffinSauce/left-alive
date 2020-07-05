954d18e7757b8d6bcd3246a6798edc8ee4d53a6b
GriffinSauce <jorisjgriffioen@gmail.com>
2020-07-05 23:36:17 +0200

    It's just (almost) markdown :facepalm:

--- components/AirtableContent.js
+++ components/AirtableContent.js
@@ -1,39 +1,10 @@
-const linksListRegex = /^  \[(?<id>[0-9])\]: (?<url>.*)/gm;
+const ReactMarkdown = require('react-markdown');
 
-/**
- * Links are encoded in a list at the end
- */
-const getLinks = (content) => {
-  const links = {};
-  let match;
-  while ((match = linksListRegex.exec(content))) {
-    const { id, url } = match.groups;
-    links[id] = url.replace('http:///', '/'); // Airtable doesn't keep relative links
-  }
-  return links;
-};
-
-const renderLink = ({ text, href }) =>
-  `<a target="${
-    href.startsWith('http') ? '_blank' : ''
-  }" rel="noopener noreferrer" href=${href}>${text}</a>`;
-
-const AirtableContent = ({ content = '' }) => {
-  const links = getLinks(content);
-
+const AirtableContent = ({ content = '', className = '' }) => {
   const formatted = content
-    .replace(linksListRegex, ``) // Remove links list
-    .replace(/\[(.*)\]\[([0-9])\]/gm, (match, text, linkId) =>
-      renderLink({ text, href: links[linkId] }),
-    ) // Inline links
-    .replace(/\*\*(.*)\*\*/g, `<b>$1</b>`); // Bold
-
-  const paragraphs = formatted
-    .split('\n')
-    .filter(Boolean)
-    .map((p) => <p dangerouslySetInnerHTML={{ __html: p }} />);
-
-  return paragraphs;
+    .replace('http:///', '/') // Airtable doesn't keep relative links
+    .replace(/\n/gm, '\n\n'); // Airtable doesn't seem to render paragraph breaks right
+  return <ReactMarkdown className={className} source={formatted} escapeHtml />;
 };
 
 export default AirtableContent;
