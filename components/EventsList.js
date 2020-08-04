dfd86e5568113850b6c1002864ea5b149de3a32b
GriffinSauce <jorisjgriffioen@gmail.com>
2020-08-04 19:53:51 +0200

    Add more event props

--- components/EventsList.js
+++ components/EventsList.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
-import { FiMap, FiChevronDown } from 'react-icons/fi';
+import { FiClock, FiMap, FiChevronDown, FiExternalLink } from 'react-icons/fi';
 import { RiMoneyEuroCircleLine } from 'react-icons/ri';
 import { parseISO, format } from 'date-fns';
-import AirtableContent, { isEmptyContent } from './AirtableContent';
+import AirtableContent from './AirtableContent';
 import useDimensions from '../hooks/useDimensions';
 
 const renderAddress = (address, city) =>
@@ -14,9 +14,7 @@ const Event = ({ event }) => {
   const maxHeight = 350;
   const [descriptionRef, { height }] = useDimensions();
   const [isCollapsed, setIsCollapsed] = useState(height > maxHeight);
-  useEffect(() => {
-    setIsCollapsed(height > maxHeight);
-  }, [height]);
+  useEffect(() => setIsCollapsed(height > maxHeight), [height]);
 
   const date = parseISO(event.date);
 
@@ -32,49 +30,68 @@ const Event = ({ event }) => {
         </h1>
 
         <div className="flex flex-col-reverse grid-flow-col grid-cols-2 sm:grid">
-          {!isEmptyContent(event.publicDescription) ? (
-            <section className="p-4 text-base bg-gray-100">
-              <div className={isCollapsed ? 'description-truncate' : ''}>
-                <AirtableContent
-                  ref={descriptionRef}
-                  className="grid gap-3 break-words"
-                  content={event.publicDescription}
-                />
-              </div>
-              {isCollapsed ? (
-                <button
-                  type="button"
-                  className="flex flex-col items-center justify-center w-full p-3 pb-0 border-t border-gray-300"
-                  onClick={() => setIsCollapsed(false)}
-                >
-                  <span>read more</span>
-                  <FiChevronDown />
-                </button>
-              ) : null}
-            </section>
-          ) : null}
-          <a
-            className="block p-4 bg-gray-100 border-b sm:border-l sm:border-b-0"
-            target="_blank"
-            rel="noopener noreferrer"
-            href={`https://maps.google.com/maps?f=q&q=${mapsEncode(
-              renderAddress(event.venue.address, event.venue.city),
-            )}`}
-          >
-            <div className="mb-3 font-sans font-bold">{event.venue.name}</div>
+          <section className="p-4 text-base bg-gray-100">
+            <div className={isCollapsed ? 'description-truncate' : ''}>
+              <AirtableContent
+                ref={descriptionRef}
+                className="grid gap-3 break-words"
+                content={event.publicDescription}
+              />
+            </div>
+            {isCollapsed ? (
+              <button
+                type="button"
+                className="flex flex-col items-center justify-center w-full p-3 pb-0 border-t border-gray-300"
+                onClick={() => setIsCollapsed(false)}
+              >
+                <span>read more</span>
+                <FiChevronDown />
+              </button>
+            ) : null}
+          </section>
+          <section className="block py-2 bg-gray-100 border-b sm:border-l sm:border-b-0">
+            <a
+              className="block px-4 py-2"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={`https://maps.google.com/maps?f=q&q=${mapsEncode(
+                renderAddress(event.venue.address, event.venue.city),
+              )}`}
+            >
+              <div className="mb-3 font-sans font-bold">{event.venue.name}</div>
 
-            <div className="flex items-center mb-3">
-              <FiMap className="mr-2" />
-              <span>
-                {renderAddress(event.venue.address, event.venue.city)}
-              </span>
+              <div className="flex items-center">
+                <FiMap className="mr-2" />
+                <span>
+                  {renderAddress(event.venue.address, event.venue.city)}
+                </span>
+              </div>
+            </a>
+
+            <div className="flex items-center px-4 py-2 text-primary-500">
+              <FiClock className="mr-2" />
+              <span>{event.doors}</span>
             </div>
 
-            <div className="flex items-center mb-3">
+            <div className="flex items-center px-4 py-2 text-primary-500">
               <RiMoneyEuroCircleLine className="mr-2" />
               <span>{event.price}</span>
             </div>
-          </a>
+
+            {event.eventLink ? (
+              <a
+                className="flex items-center px-4 py-2"
+                target="_blank"
+                rel="noopener noreferrer"
+                href={event.eventLink}
+              >
+                <FiExternalLink className="flex-shrink-0 mr-2" />
+                <span className="truncate">
+                  {event.eventLink.replace(/^http(s)?:\/\/(www\.)?/, '')}
+                </span>
+              </a>
+            ) : null}
+          </section>
         </div>
       </li>
       <style jsx>{`
