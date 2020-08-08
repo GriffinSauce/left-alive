02f18273fb79e1799cef36fa706dc3edcbff4d75
Joris Griffioen <jorisjgriffioen@gmail.com>
2020-08-08 15:04:03 +0200

    Save .4KB of classnames by scoping truncate into component

--- components/ShowsList.js
+++ components/ShowsList.js
@@ -1,12 +1,11 @@
-import { useState, useEffect } from 'react';
-import { FiClock, FiMap, FiChevronDown, FiExternalLink } from 'react-icons/fi';
+import { FiClock, FiMap, FiExternalLink } from 'react-icons/fi';
 import { RiMoneyEuroCircleLine } from 'react-icons/ri';
 import { parseISO, format } from 'date-fns';
 import ModalImage from 'react-modal-image';
 import AirtableContent from './AirtableContent';
 import ShowStructuredData from './ShowStructuredData';
 import Anchor from './Anchor';
-import useDimensions from '../hooks/useDimensions';
+import Truncate from './Truncate';
 
 const renderAddress = (address, city) =>
   [address, city].filter(Boolean).join(', ');
@@ -18,18 +17,12 @@ const isImageAttachment = (attachment) => attachment.type.startsWith('image');
 const Show = ({ show }) => {
   if (!show.title || !show.date) return null; // Not a show then is it?!
 
-  const maxHeight = 350;
-  const [descriptionRef, { height }] = useDimensions();
-  const [isCollapsed, setIsCollapsed] = useState(height > maxHeight);
-  useEffect(() => setIsCollapsed(height > maxHeight), [height]);
-
   const date = parseISO(show.date);
-
   const thumbnails = show.images?.filter(isImageAttachment)?.[0]?.thumbnails;
 
   return (
     <>
-      <li className="grid w-full gap-3 break-words bg-white">
+      <li className="grid w-full gap-3">
         <Anchor id={show.id} />
         <ShowStructuredData show={show} />
         <h1 className="flex flex-col sm:block">
@@ -51,25 +44,14 @@ const Show = ({ show }) => {
               />
             ) : null}
 
-            <div className={isCollapsed ? 'description-truncate' : ''}>
+            <Truncate maxHeight={350}>
               <AirtableContent
-                ref={descriptionRef}
                 className="grid gap-3 break-words"
                 content={show.publicDescription}
               />
-            </div>
-            {isCollapsed ? (
-              <button
-                type="button"
-                className="flex flex-col items-center justify-center w-full p-3 pb-0 border-t border-gray-300"
-                onClick={() => setIsCollapsed(false)}
-              >
-                <span>read more</span>
-                <FiChevronDown />
-              </button>
-            ) : null}
+            </Truncate>
           </section>
-          <section className="block py-2 bg-gray-100 border-b sm:border-l sm:border-b-0">
+          <section className="block py-2 bg-gray-100 border-b text-primary-500 sm:border-l sm:border-b-0">
             <a
               className="block px-4 py-2"
               target="_blank"
@@ -88,12 +70,12 @@ const Show = ({ show }) => {
               </div>
             </a>
 
-            <div className="flex items-center px-4 py-2 text-primary-500">
+            <div className="flex items-center px-4 py-2">
               <FiClock className="mr-2" />
               <span>{show.doors}</span>
             </div>
 
-            <div className="flex items-center px-4 py-2 text-primary-500">
+            <div className="flex items-center px-4 py-2">
               <RiMoneyEuroCircleLine className="mr-2" />
               <span>{show.price}</span>
             </div>
@@ -116,35 +98,13 @@ const Show = ({ show }) => {
           </section>
         </div>
       </li>
-      <style jsx>{`
-        .description-truncate {
-          position: relative;
-          overflow-y: hidden;
-          max-height: ${maxHeight}px;
-        }
-
-        .description-truncate:after {
-          position: absolute;
-          display: block;
-          content: '';
-          width: 100%;
-          height: 50px;
-          background: linear-gradient(
-            0deg,
-            rgba(247, 250, 252, 1) 0%,
-            rgba(247, 250, 252, 0) 100%
-          );
-          bottom: 0;
-          left: 0;
-        }
-      `}</style>
     </>
   );
 };
 
 const ShowsList = ({ shows }) => {
   return (
-    <ul className="grid gap-10 sm:gap-20">
+    <ul className="grid gap-10 break-words bg-white sm:gap-20">
       {shows.map((show) => (
         <Show key={show.id} show={show} />
       ))}
---
+++ components/Truncate.js
@@ -0,0 +1,51 @@
+import { FiChevronDown } from 'react-icons/fi';
+import { useState, useEffect } from 'react';
+import useDimensions from '../hooks/useDimensions';
+
+const Truncate = ({ children, maxHeight }) => {
+  const [descriptionRef, { height }] = useDimensions();
+  const [isCollapsed, setIsCollapsed] = useState(height > maxHeight);
+  useEffect(() => setIsCollapsed(height > maxHeight), [height]);
+
+  return (
+    <>
+      <div className={isCollapsed ? 'description-truncate' : ''}>
+        <div ref={descriptionRef}>{children}</div>
+      </div>
+      {isCollapsed ? (
+        <button
+          type="button"
+          className="flex flex-col items-center justify-center w-full p-3 pb-0 border-t border-gray-300"
+          onClick={() => setIsCollapsed(false)}
+        >
+          <span>read more</span>
+          <FiChevronDown />
+        </button>
+      ) : null}
+      <style jsx>{`
+        .description-truncate {
+          position: relative;
+          overflow-y: hidden;
+          max-height: ${maxHeight}px;
+        }
+
+        .description-truncate:after {
+          position: absolute;
+          display: block;
+          content: '';
+          width: 100%;
+          height: 50px;
+          background: linear-gradient(
+            0deg,
+            rgba(247, 250, 252, 1) 0%,
+            rgba(247, 250, 252, 0) 100%
+          );
+          bottom: 0;
+          left: 0;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default Truncate;
