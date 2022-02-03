474484996b06fe8970cd48097b0224bd50edec3f
Joris Griffioen <jorisjgriffioen@gmail.com>
2022-02-03 20:44:36 +0100

    Fix container style

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     container: {
       center: true,
       padding: {
-        default: '1rem',
+        DEFAULT: '1rem',
         sm: '2rem',
         lg: '4rem',
         xl: '10rem',
